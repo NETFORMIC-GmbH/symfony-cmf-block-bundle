9b325e5d0ed4b39e28f6ba2e394f4a673399364f
Philipp Gildein <philipp.gildein@netformic.com>
2021-08-12 14:14:34 +0200

    Fix type of ManagerRegistry

--- src/Block/PhpcrBlockLoader.php
+++ src/Block/PhpcrBlockLoader.php
@@ -11,7 +11,7 @@
 
 namespace Symfony\Cmf\Bundle\BlockBundle\Block;
 
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use Psr\Log\LoggerInterface;
 use Sonata\BlockBundle\Block\BlockLoaderInterface;
 use Sonata\BlockBundle\Exception\BlockNotFoundException;
